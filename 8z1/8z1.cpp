70dd10a238475d0e6d73723788b6ef18f53acb39
SystemX <SystemX@DESKTOP-V0TMCHI>
2026-04-21 07:54:59 +0300

    8z1

---
+++ 8z1/8z1.cpp
@@ -0,0 +1,69 @@
+﻿//ИПБ-25 Скворцов С.А. 3.8 1
+/*1. Написать программу формирования списка L1 из вводимых с клавиатуры целых чисел, а затем формирует список L2, в который помещает только положительные элементы из L1 и выводит список L2 на экран.*/
+
+#include <iostream>
+//#include <forward_list>
+//#include "rus_io.h"
+//#include "conio2.h"
+using namespace std;
+
+struct Node
+{
+    Node* next;
+    int val;
+};
+void pushStack(Node* top, int last)
+{
+    Node q;
+    q.val = last;
+    q.next = top;
+    top = &q;
+};
+struct FIFO
+{
+    Node* first;
+    Node* last;
+};
+void popOrd(FIFO& queue, int& first) 
+{ 
+    Node* q = queue.first->next; 
+    first = q->val; 
+    queue.first->next = q->next; 
+    delete q; 
+    if (!queue.first->next) 
+        queue.last = queue.first; 
+}
+int main()
+{
+    //setRusLocale();
+    cout << "Введите кол-во чисел ";
+    int n;
+    cin >> n;
+    cout << "Вводите числа\n";
+    Node *l1 = new Node, *l2 = new Node;
+    Node *cur = l1;
+    for (int i = 0; i < n; i++)
+    {
+        cur->next = new Node;
+        cur = cur->next;
+        cin >> cur->val;
+    }
+    cur = l2;
+    Node *curl1 = l1;
+    do {
+        curl1 = curl1->next;
+        if (curl1->val > 0)
+        {
+            cur->next = new Node;
+            cur = cur->next;
+            cur->val = curl1->val;
+        }
+    } while (curl1->next);
+    cur = l2;
+    do{
+        cur = cur->next;
+        cout << cur->val << " ";
+    } while (cur->next);
+    system("PAUSE");
+    return 0;
+}
---
+++ 8z2/8z2.cpp
@@ -0,0 +1,54 @@
+﻿//ИПБ-25 Скворцов С.А. 3.8 2
+/*Включить в  список упорядоченных по возрастанию целых чисел   произвольное  новое  число  без  нарушения  упорядоченности и  исключите  другое  указанное  число (если такое число имеется в списке)*/
+
+#include <iostream>
+#include <forward_list>
+//#include "rus_io.h"
+//#include "conio2.h"
+using namespace std;
+
+
+int main()
+{
+    //setRusLocale();
+    cout << "Введите кол-во чисел ";
+    int n;
+    cin >> n;
+    cout << "Вводите числа\n";
+    forward_list<int> l;
+    for (int i = 0; i < n; i++)
+    {
+        int cur;
+        cin >> cur;
+        l.push_front(cur);
+    }
+    int addNum, delNum;
+    bool isAdd = false, isDel = false;
+    cin >> addNum >> delNum;
+    auto prev = l.before_begin();
+    auto cur = l.begin();
+    auto end = l.end();
+    if (addNum > *cur)
+    {
+        l.insert_after(prev, addNum);
+        isAdd = true;
+    }
+    prev++;
+    cur++;
+    while(cur!=end)
+    {
+        if (!isAdd && *prev >= addNum && addNum > *cur)
+        {
+            l.insert_after(cur, addNum);
+            isAdd = true;
+        }
+        if (*cur == delNum)
+        {
+
+        }
+        prev++;
+        cur++;
+    }
+    system("PAUSE");
+    return 0;
+}
